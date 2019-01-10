99a44f069f9d1884d63449a777158ab711dd12c7
alisonuij <46568391+alisonuij@users.noreply.github.com>
2019-01-10 13:05:03 -0200

    Add files via upload

---
+++ novo projeto/_cabecario.php
@@ -0,0 +1,27 @@
+<?php
+//--------------------
+//cabecalho
+//--------------------
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>projeto 1</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="inc/main.css">
+
+</head>
+<body>
+    
+</body>
+</html>
---
+++ novo projeto/index.php
@@ -0,0 +1,13 @@
+<?php
+
+//--------------------
+//index
+//--------------------
+
+include_once('_cabecario.php');
+
+echo '<p>teste de fontawe <span class="fa fa-times"></span></p>';
+
+include_once('_rodape.php');
+
+?>
